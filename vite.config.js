77bac7938089bffa5a87810f043ce16f6ed92d85
probeike <agouws@macbookpro.mynetworksettings.com>
2025-04-10 15:12:19 -0400

    Added deploy action for github deployment to pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base path for GitHub Pages deployment
+  // In production, it will be set to the repository name
+  // In development, it will be '/'
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
 })
